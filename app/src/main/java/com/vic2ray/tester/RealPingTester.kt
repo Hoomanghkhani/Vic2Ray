fddb5c2812f365076a98bdf1e8164e8e8d3267f1
Houman Ghardashkhani <h.ghardashkhani@zarin.local>
2026-06-21 11:22:53 +0330

    Use IP in ping tester to avoid DNS resolution timeout

--- app/src/main/java/com/vic2ray/tester/RealPingTester.kt
+++ app/src/main/java/com/vic2ray/tester/RealPingTester.kt
@@ -13,7 +13,8 @@ object RealPingTester {
         try {
             // V2Ray default local HTTP proxy in this app
             val proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress("127.0.0.1", 10809))
-            val url = URL("http://gstatic.com/generate_204")
+            // Use an IP address directly to avoid local DNS resolution timing out in tun0 blackhole
+            val url = URL("http://1.1.1.1/")
             val connection = url.openConnection(proxy) as HttpURLConnection
             connection.connectTimeout = 3000
             connection.readTimeout = 3000
@@ -22,7 +23,8 @@ object RealPingTester {
             val responseCode = connection.responseCode
             val endTime = System.currentTimeMillis()
             
-            if (responseCode == 204 || responseCode == 200) {
+            // Any response means the proxy is working and reachable
+            if (responseCode > 0) {
                 return@withContext (endTime - startTime).toInt()
             } else {
                 return@withContext -1
